ba6e291c6234b2c144817860ccd087ad80376f93
Alexander Joshua <alexander.joshua2018@gmail.com>
2022-02-23 00:14:36 +0100

    fix

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -17,7 +17,9 @@ const NavigationDots = ({ active }) => {
           key={item + index}
           className="app__navigation-dot"
           style={active === item ? { backgroundColor: "#313BAC" } : {}}
-        />
+        >
+          {" "}
+        </a>
       ))}
     </div>
   );
